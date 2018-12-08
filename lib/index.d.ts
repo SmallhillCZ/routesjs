921041142d2f2391c03d4a457a55b432121478f0
SmallhillCZ <github@smallhill.cz>
2018-12-08 13:21:35 +0000

    Fix bad export

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,4 @@
 export { Routes } from "./routes";
+export { RoutesACL } from "./acl";
 export { RoutesLinksRoot } from "./links-root";
 export * from "./interfaces";
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-export { Routes, RoutesACL } from "./routes";
+export { Routes } from "./routes";
+
+export { RoutesACL } from "./acl";
 
 export { RoutesLinksRoot } from "./links-root";
 
