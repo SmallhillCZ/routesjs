033d08f522ea948e55d80a8b778b75b362512f8a
SmallhillCZ <github@smallhill.cz>
2018-12-18 14:41:45 +0000

    Update mongoose plugin query filter

--- src/plugins/mongoose.ts
+++ src/plugins/mongoose.ts
@@ -2,22 +2,27 @@ import { RoutesACL } from "../acl";
 
 import * as express from "express";
 
+function filterByPermission(permission:string,req:express.Request,throwError:boolean){
+
+  const aclResult = RoutesACL.can(permission,req);
+
+  if(!aclResult.allowed){
+    if(throwError) {
+      console.log("throw");
+      throw new Error("Unauthorized");
+    }
+    else this.where({nonexistentrouteswherevariable:5});
+  }
+  if(aclResult.filters.length) this.where({ $or: aclResult.filters });
+
+  return this;
+}
+
 export function RoutesPluginsMongoose(schema){
 
-  schema.query.permission = function(permission:string,req:express.Request,throwError:boolean){
-    
-    const aclResult = RoutesACL.can(permission,req);
-    
-    if(!aclResult.allowed){
-      if(throwError) {
-        console.log("throw");
-        throw new Error("Unauthorized");
-      }
-      else this.where({nonexistentrouteswherevariable:5});
-    }
-    if(aclResult.filters.length) this.where({ $or: aclResult.filters });
-    
-    return this;
-  };
+  schema.query.filterByPermission = filterByPermission;
   
+  // old alias
+  schema.query.permission = filterByPermission;
+
 };
