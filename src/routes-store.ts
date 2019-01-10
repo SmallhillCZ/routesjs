113b86cc83d93e4941e5673f48bf8495e7f5082a
SmallhillCZ <github@smallhill.cz>
2019-01-10 22:06:34 +0000

    Return 403 on bad permissions.

--- src/routes-store.ts
+++ src/routes-store.ts
@@ -6,7 +6,8 @@ export const routesStore:RoutesStore = {
   routes: [],
   acl: {
     permissions: {},
-    userRoles: (req:express.Request) => []
+    userRoles: (req:express.Request) => [],
+    authenticated: (req:express.Request) => false
   },
   options: {},
 }
